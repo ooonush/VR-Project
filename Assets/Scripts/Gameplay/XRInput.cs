722563da515d701f491a56797666181f9884c50b
Анушервон <ooonush@gmail.com>
2022-12-04 20:41:20 +0500

    Input refactorin

--- Assets/Scripts/Gameplay/XRInput.cs
+++ Assets/Scripts/Gameplay/XRInput.cs
@@ -1,7 +1,30 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
 
 namespace Gameplay
 {
+    public class DoorInteractable : XRBaseInteractable
+    {
+        private Vector3 _startPosition;
+        
+        protected override void OnSelectEntered(SelectEnterEventArgs args)
+        {
+            base.OnSelectEntered(args);
+
+            _startPosition = transform.position;
+        }
+
+        public override void ProcessInteractable(XRInteractionUpdateOrder.UpdatePhase updatePhase)
+        {
+            base.ProcessInteractable(updatePhase);
+
+            IXRSelectInteractor interactor = interactorsSelecting[0];
+            Vector3 position = transform.position;
+        }
+    }
+    
+    
     public class XRInput : MonoBehaviour
     {
         public XRControllerInput RightHandControllerInput;
@@ -16,13 +39,24 @@ namespace Gameplay
         private void Awake()
         {
             _inputActions = new XRIDefaultInputActions();
-            _inputActions.Enable();
             
             XRIDefaultInputActions.XRILeftHandLocomotionActions leftHandLocomotion = _inputActions.XRILeftHandLocomotion;
             
             TeleportModeActivateAction = new XRInputAction<Vector2>(leftHandLocomotion.TeleportModeActivate);
             TeleportModeCancelAction = new XRInputAction(leftHandLocomotion.TeleportModeCancel);
             MoveAction = new XRInputAction<Vector2>(leftHandLocomotion.Move);
+
+            TeleportModeActivateAction.OnPerformed += _ => Debug.Log("TeleportModeActivateAction");
+            TeleportModeCancelAction.OnPerformed += () => Debug.Log("TeleportModeCancelAction");
+            MoveAction.OnPerformed += _ => Debug.Log("MoveAction");
+
+            RightHandControllerInput.SelectAction.Action.performed += (_) => Debug.Log("RightHandControllerInput.SelectAction");
+            LeftHandControllerInput.SelectAction.Action.performed += (_) => Debug.Log("LeftHandControllerInput.SelectAction");
+        }
+
+        private void Start()
+        {
+            _inputActions.Enable();
         }
     }
 }
